17689563100ce9a378ebc1e116a30265849eb73a
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-29 12:08:27 +0200

    Bug css .nav-l

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,63 +1,76 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import logoCycle from "../../assets/cycle.svg";
+import logoFitness from "../../assets/fitness.svg";
+import logoSwimming from "../../assets/swimming.svg";
+import logoYoga from "../../assets/yoga.svg";
 
 function NavBar() {
   let activeStyle = {
     textDecoration: "underline",
   };
   return (
-    <header className="header">
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Logo SportSee" className="logoHeader"></img>
-        </Link>
-      </div>
-      <nav className="nav-t">
-        <NavLink
-          className="navHome"
-          to="/"
-          style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        >
-          Accueil
-        </NavLink>
-        <NavLink
-          className="navProfile"
-          to="/profil"
-          style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        >
-          Profil
-        </NavLink>
-        <NavLink
-          className="navSetting"
-          to="/reglage"
-          style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        >
-          Réglage
-        </NavLink>
-        <NavLink
-          className="navCommunity"
-          to="/communite"
-          style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        >
-          Communauté
-        </NavLink>
-      </nav>
+    <div className="NavBar">
+      <header className="header">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo SportSee" className="logoHeader"></img>
+          </Link>
+        </div>
+        <nav className="nav-t">
+          <NavLink
+            className="navHome"
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Accueil
+          </NavLink>
+          <NavLink
+            className="navProfile"
+            to="/profil"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Profil
+          </NavLink>
+          <NavLink
+            className="navSetting"
+            to="/reglage"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Réglage
+          </NavLink>
+          <NavLink
+            className="navCommunity"
+            to="/communite"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Communauté
+          </NavLink>
+        </nav>
+      </header>
       <nav className="nav-l">
-        <NavLink className="bt-yoga" to="/">
-          yoga
-        </NavLink>
-        <NavLink className="bt-swimming" to="/">
-          swimming
-        </NavLink>
-        <NavLink className="bt-cycle" to="/">
-          cycle
-        </NavLink>
-        <NavLink className="bt-fitness" to="/">
-          fitness
-        </NavLink>
+        <div className="nav-l-group">
+          <NavLink className="bt-yoga nav-l-bt" to="/">
+            <img src={logoYoga} alt="Logo Kasa" className="logoHeader"></img>
+          </NavLink>
+          <NavLink className="bt-swimming nav-l-bt" to="/">
+            <img
+              src={logoSwimming}
+              alt="Logo Kasa"
+              className="logoHeader"
+            ></img>
+          </NavLink>
+          <NavLink className="bt-cycle nav-l-bt" to="/">
+            <img src={logoCycle} alt="Logo Kasa" className="logoHeader"></img>
+          </NavLink>
+          <NavLink className="bt-fitness nav-l-bt" to="/">
+            <img src={logoFitness} alt="Logo Kasa" className="logoHeader"></img>
+          </NavLink>
+        </div>
+        <p>Copiryght, SportSee 2022</p>
       </nav>
-    </header>
+    </div>
   );
 }
 
