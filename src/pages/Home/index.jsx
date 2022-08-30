dfa022d50d47e57bac26ac591f23986bc911f052
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-08-30 03:42:54 +0200

    Axios lib + home init

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Home() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch("/dataMoked.json", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        });
+        if (!response.ok) {
+          throw new Error(` - Error status ${response.status}`);
+        }
+        let actualData = await response.json();
+        setData(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getData();
+  }, []);
+}
