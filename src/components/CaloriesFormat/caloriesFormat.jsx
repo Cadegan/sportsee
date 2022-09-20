a3bb96c156d42d9fcd138e0a82c4061a3ed03f8d
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-09-20 06:12:31 +0200

    Readme v4

--- src/components/CaloriesFormat/caloriesFormat.jsx
+++ src/components/CaloriesFormat/caloriesFormat.jsx
@@ -1,5 +1,5 @@
 /**
- * It takes a number as an argument and returns a formatted number
+ * It takes a number as an argument and returns a formatted number X.XXX
  */
 const numberFormat = (value) => new Intl.NumberFormat("en-IN").format(value);
 
