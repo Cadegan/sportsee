23132adc3a18fb315320d862923b782838dc99a8
Cadegan <91974649+Cadegan@users.noreply.github.com>
2022-09-30 14:36:53 +0200

    Nettoyage 🧹

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/main.css";
-// import { UserProvider } from "./utils/context/context";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <UserProvider> */}
     <App />
-    {/* </UserProvider> */}
   </React.StrictMode>
 );
 
